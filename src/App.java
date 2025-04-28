8da80745749ec8445e23d49cca1e6782db492f13
Ynot-DevCreator <ead@academico.local>
2025-04-28 11:08:45 -0300

    denovo

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Alô, Git!");
+        System.out.println("\n\t\tJEstatistics");
+        System.out.print("-- Controle de Dados Empresarial -- ");
+        System.out.println("2025n\n");
     }
 }
