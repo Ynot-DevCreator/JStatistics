4a9571fe525d30781e1862471adc1f96b959f0e2
Ynot-DevCreator <ead@academico.local>
2025-04-30 11:55:33 -0300

    logica-aula2 finalizada

--- src/App.java
+++ src/App.java
@@ -1,7 +1,47 @@
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("\n\t\tJEstatistics");
-        System.out.print("-- Controle de Dados Empresarial -- ");
-        System.out.println("2025n\n");
+
+        /*int op = Integer.parseInt(JOptionPane.showInputDialog("Qual o Resultado de uma população?\n 1-Parâmetro\n 2-Estatística"));
+            if (op == 1) {
+                JOptionPane.showMessageDialog(null, "Certa Resposta");
+            } else {
+                JOptionPane.showMessageDialog(null, "Resposta Errada!", "JStatistics",JOptionPane.ERROR_MESSAGE);
+            }*/
+        
+            //if else
+            // Exercícios   (para cada pergunta crie um if)
+            /*Considerando os números 30, 50, 80, 90 Digite o valor da média aritmética simples */
+            //Gabarito: 62.5
+            /*Considerando os valores 10, 40, 50, 80, 10. Digite qual é a moda? */
+            //Gabarito: 10
+            /*Qual a medida de tendência central que mostra o centro da amostra?  */
+            //Gabarito: mediana
+
+            // String nome;
+            // if nome.equals("mediana")
+        double media = Double.parseDouble(JOptionPane.showInputDialog("Considerando os números:\n 30, 50, 80, 90 \nDigite o valor da Média Aritmética simples"));
+            if (media == 62.5) {
+                JOptionPane.showMessageDialog(null, "Resposta Correta!");
+            }else {
+                JOptionPane.showMessageDialog(null, "Resposta Errada!", "JStatistics",JOptionPane.ERROR_MESSAGE);
+            }
+        int moda = Integer.parseInt(JOptionPane.showInputDialog("Considerando os valores: \n10, 40, 50, 80, 10 \nDigite qual é a moda?"));
+        if (moda == 10) {
+            JOptionPane.showMessageDialog(null, "Resposta Correta!");
+        }else {
+            JOptionPane.showMessageDialog(null, "Resposta Errada!", "JStatistics",JOptionPane.ERROR_MESSAGE);
+        }
+        String mediana = JOptionPane.showInputDialog("Qual a medida de tendência central que mostra o centro da amostra?");
+        if (mediana.equalsIgnoreCase("mediana")) {
+            JOptionPane.showMessageDialog(null, "Resposta Correta!");
+        }else {
+            JOptionPane.showMessageDialog(null, "Resposta Errada!", "JStatistics",JOptionPane.ERROR_MESSAGE);
+        }
+
+        if (media == 62.5 && moda == 10 && mediana.equalsIgnoreCase("mediana")) {
+            JOptionPane.showMessageDialog(null, "Parabéns, Você Gabaritou o Quiz! ");
+        }
     }
 }
