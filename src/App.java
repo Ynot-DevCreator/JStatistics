f1a8de5634319028388316276e5fab8ac354d0bc
Ynot-DevCreator <ead@academico.local>
2025-05-08 11:39:08 -0300

    finalizado logica aula 6

--- src/App.java
+++ src/App.java
@@ -1,7 +1,23 @@
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("\n\t\tJEstatistics");
-        System.out.print("-- Controle de Dados Empresarial -- ");
-        System.out.println("2025n\n");
+        //Classe é um conjunto de objetos
+        //Nosso objetivo nesse código é entender a diferença
+        //entre método com o tipo void e método com o tipo String
+        Mamifero m1 = new Mamifero();
+        Mamifero m2 = new Mamifero();
+        Mamifero m3 = new Mamifero();
+        m1.setNomeCientifico("Bos Taurus");
+        m2.setNomeCientifico("Macropus");
+        m3.setNomeCientifico("Trichechus");
+        JOptionPane.showMessageDialog(null, "Nome Científico:\n"+m1.getNomeCientifico()+"\n"+m2.getNomeCientifico()+"\n"+m3.getNomeCientifico());
+        //Exercício
+        /* 
+         * Considerando a utilização de métodos set e get
+         * Considerando a aula de hoje
+         * Diferença entre função e método
+         */
+
     }
 }
---
+++ src/Mamifero.java
@@ -0,0 +1,15 @@
+public class Mamifero {
+    //variáveis = atributos
+    //características
+    private int id; // atributo do tipo Número
+    private String nomeCientifico; //atributo do tipo do Texto
+    public int getId(){
+        return this.id;
+    }
+    public void setNomeCientifico(String nomeCientifico){
+        this.nomeCientifico = nomeCientifico;
+    }
+    public String getNomeCientifico(){
+        return this.nomeCientifico;
+    }
+}
