b5b0a9b14b7b25f98751e0ec2c4d4dfd8e2229d9
Ynot-DevCreator <ead@academico.local>
2025-05-09 08:32:27 -0300

    Início

--- src/App.java
+++ src/App.java
@@ -1,10 +1,7 @@
-import java.util.ArrayList; // teste
+import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("\n\t\tJEstatistics");
-        System.out.print("-- Controle de Dados Empresarial -- ");
-        System.out.println("2025n\n");
-        // ArrayList lista = new ArrayList();
+        JOptionPane.showMessageDialog(null, "Alô, Jardim", "Começo", JOptionPane.INFORMATION_MESSAGE);
     }
 }
